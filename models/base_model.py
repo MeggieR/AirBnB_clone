5e169742b9fe06cc9872daa02371024acb421df7
MeggieR <mrotrigs@gmail.com>
2024-05-19 02:24:20 -0700

    updated basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -2,19 +2,27 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-        
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
     def __str__(self):
         """Returns string representation of the object"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
+
     def save(self):
         """Updates the updated_at attribute with the current datetime"""
         self.updated_at = datetime.now()
-        
+
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__ of the instance"""
         dict_json = self.__dict__.copy()
